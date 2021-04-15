5dc3a9d5d888797c38d7ca5331161ac72694d130
logchan <logchan@outlook.com>
2021-04-15 18:28:42 -0400

    update

--- sensor-pollster-web-client/src/components/SensorGraph.jsx
+++ sensor-pollster-web-client/src/components/SensorGraph.jsx
@@ -1,17 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Grid } from '@material-ui/core'
 import {
   Charts,
   ChartContainer,
   ChartRow,
   YAxis,
   LineChart,
-  styler
+  styler,
+  Legend
 } from "react-timeseries-charts";
 import { TimeSeries } from "pondjs";
+
+// https://github.com/esnet/react-timeseries-charts/blob/aa9c9b368100d78337b562d9e2833f2d90d9de3d/src/website/packages/charts/examples/currency/Index.js#L60
+class CrossHairs extends React.Component {
+  render() {
+      const { x, y } = this.props;
+      const style = { pointerEvents: "none", stroke: "#ccc" };
+      if (x !== null && y !== null) {
+          return (
+              <g>
+                  <line style={style} x1={0} y1={y} x2={this.props.width} y2={y} />
+                  <line style={style} x1={x} y1={0} x2={x} y2={this.props.height} />
+              </g>
+          );
+      } else {
+          return <g />;
+      }
+  }
+}
 
 export default function SensorGraph(props) {
   const sensors = props.data
   const getter = props.getter
+  const [ mouseState, setMouseState ] = useState({
+    x: null,
+    y: null
+  })
+  const [ trackerState, setTrackerState ] = useState({
+    tracker: null
+  })
 
   const style = styler(sensors.map(s => {return {
     key: s.name,
@@ -23,23 +50,64 @@ export default function SensorGraph(props) {
   const max = all_values[all_values.length - 1]
   const min = all_values[0]
 
-  return <ChartContainer timeRange={new TimeSeries({
-    columns: ["time"],
-    points: [[props.start.getTime()], [props.end.getTime()]]
-  }).timerange()} width={props.width}>
-  <ChartRow height="200">
-      <YAxis id="axis" label={props.label} min={min} max={max} width="60" type="linear" format=".2f" tickCount={10}/>
-      <Charts>
-        {sensors.map(s => {
-          return <LineChart key={s.address} axis="axis" series={new TimeSeries({
-            name: s.name,
-            columns: ["time", s.name],
-            points: s.data.map(d => [new Date(d.time).getTime(), getter(d)]).reverse()
-          })}
-          style={style}
-          columns={[s.name]}/>
-        })}
-      </Charts>
-  </ChartRow>
-</ChartContainer>
+  const all_series = {}
+  sensors.forEach(s => {
+    all_series[s.name] = new TimeSeries({
+      name: s.name,
+      columns: ["time", s.name],
+      points: s.data.map(d => [new Date(d.time).getTime(), getter(d)]).reverse()
+    })
+  })
+
+  const tracker_values = {}
+  if (trackerState.tracker) {
+    sensors.forEach(s => {
+      const series = all_series[s.name]
+      tracker_values[s.name] = series.at(series.bisect(trackerState.tracker)).get(s.name).toString()
+    })
+  }
+
+  return <div>
+    <Grid container><Grid item sm={12}>
+      <ChartContainer timeRange={new TimeSeries({
+            columns: ["time"],
+            points: [[props.start.getTime()], [props.end.getTime()]]
+          }).timerange()}
+          onTrackerChanged={tracker => { 
+            setTrackerState({tracker})
+            if (tracker === null) {
+              setMouseState({x: null, y: null})
+            }
+         }}
+          width={props.width}
+          onMouseMove={(x, y) => setMouseState({x, y})}
+          timeAxisTickCount={12}
+        >
+        <ChartRow height="200">
+            <YAxis id="axis" label={props.label} min={min} max={max} 
+                    width="60" type="linear" format=".2f" tickCount={8}/>
+            <Charts>
+              {sensors.map(s => {
+                return <LineChart key={s.address} axis="axis" series={all_series[s.name]}
+                style={style}
+                columns={[s.name]}/>
+              })}
+              <CrossHairs x={mouseState.x} y={mouseState.y} />
+            </Charts>
+        </ChartRow>
+      </ChartContainer>
+    </Grid></Grid>
+    <Grid container><Grid item sm={12}>
+      <Legend
+        type="line"
+        align="left"
+        style={style}
+        categories={sensors.map(s => { return {
+          key: s.name,
+          label: s.name,
+          value: tracker_values[s.name] ?? "--"
+        }})}
+      />
+    </Grid></Grid>
+  </div>
 }
--- sensor-pollster-web-client/src/pages/Graphs.tsx
+++ sensor-pollster-web-client/src/pages/Graphs.tsx
@@ -1,4 +1,4 @@
-import { Button, Checkbox, debounce, FormControlLabel, Grid, Paper, Typography } from '@material-ui/core'
+import { Button, Checkbox, debounce, FormControlLabel, Grid, Typography } from '@material-ui/core'
 import React, { useEffect, useState } from 'react'
 import Client from '../models/Client'
 import Sensor from '../models/Sensor'
@@ -63,6 +63,15 @@ export default function Graphs() {
     })()
   })
 
+  if (!state.loaded) {
+    return <Grid container>
+    <Grid item sm={12}>
+      <Typography variant="body1">Loading</Typography>
+    </Grid>
+  </Grid>
+  }
+
+  const sensorsToDisplay = state.data.filter(s => state.showSensor.get(s.address) ?? true)
   const setRange = (offset: number) => {
     state.end = new Date()
     state.start = new Date(state.end.getTime() - offset)
@@ -76,47 +85,45 @@ export default function Graphs() {
       setRange(hoursOffset(hours))
     }}>{text}</Button></Grid>
   }
+  const makeGraph = (label: String, getter: (entry: DataEntry) => number) => {
+    return <SensorGraph data={sensorsToDisplay} info={state.floorPlan.sensors} getter={getter} 
+      start={state.start} end={state.end} width={state.divWidth}
+      label={label} />
+  }
 
   return <div>
     <Typography variant="h4">Graphs</Typography>
     {vlarge()}
-    <Grid container>
-      <Grid item sm={12}>
-      { state.loaded ?
-        <SensorGraph data={state.data.filter(s => state.showSensor.get(s.address) ?? true)} info={state.floorPlan.sensors} getter={(d: DataEntry) => d.temperature} 
-        start={state.start} end={state.end} width={state.divWidth}
-        label="Temperature" /> :
-        <Typography variant="body1">Loading</Typography> }
-      </Grid>
+    <Grid container spacing={1}>
+      { state.data.map(sensor => {
+        const info = state.floorPlan.sensors.get(sensor.address)!
+        return <Grid item><FormControlLabel key={sensor.address}
+          control={<Checkbox color="primary" defaultChecked onChange={ev => {
+            const cb = ev.target as HTMLInputElement
+            state.showSensor.set(sensor.address, cb.checked)
+            updateState()
+          }} />}
+          label={sensor.name}
+          style={{color: info.color}}
+        /></Grid>
+      }) }
     </Grid>
-    {state.loaded ?
-    <div>
-      {vlarge()}
-      <Grid container spacing={2}>
-        { state.data.map(sensor => {
-          const info = state.floorPlan.sensors.get(sensor.address)!
-          return <FormControlLabel key={sensor.address}
-            control={<Checkbox color="primary" defaultChecked onChange={ev => {
-              const cb = ev.target as HTMLInputElement
-              state.showSensor.set(sensor.address, cb.checked)
-              updateState()
-            }} />}
-            label={sensor.name}
-            style={{color: info.color}}
-          />
-        }) }
-      </Grid>
-      {vlarge()}
-      <Grid container spacing={2}>
-        {makeOffsetBtn("1 hour", 1)}
-        {makeOffsetBtn("3 hours", 3)}
-        {makeOffsetBtn("6 hours", 6)}
-        {makeOffsetBtn("12 hours", 12)}
-        {makeOffsetBtn("24 hours", 24)}
-        {makeOffsetBtn("3 days", 24*3)}
-        {makeOffsetBtn("7 days", 24*7)}
-        {makeOffsetBtn("30 days", 24*30)}
-      </Grid>
-    </div> : null }
+    {vlarge()}
+    <Grid container spacing={2}>
+      {makeOffsetBtn("1 hour", 1)}
+      {makeOffsetBtn("3 hours", 3)}
+      {makeOffsetBtn("6 hours", 6)}
+      {makeOffsetBtn("12 hours", 12)}
+      {makeOffsetBtn("24 hours", 24)}
+      {makeOffsetBtn("3 days", 24*3)}
+      {makeOffsetBtn("7 days", 24*7)}
+      {makeOffsetBtn("30 days", 24*30)}
+    </Grid>
+    {vlarge()}
+    {makeGraph("Temperature", d => d.temperature)}
+    {vlarge()}
+    {makeGraph("Battery", d => d.battery)}
+    {vlarge()}
+    {makeGraph("RSSI", d => d.rssi)}
   </div>
 }
